33a79a1c36a187b09c439c978f747bb0d614ccbb
llakshmanan <llakshmanan@marketo.com>
2018-12-19 07:05:28 +0530

    Sample Test updated Signed-off-by: lajish <lajishnambiar@gmail.com>

--- src/test/java/com/qa/tests/SampleTest.java
+++ src/test/java/com/qa/tests/SampleTest.java
@@ -1,12 +1,24 @@
 package com.qa.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SampleTest {
 
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("1. I am into BeforeMethod annotated method");
+	}
+	
 	@Test
-	public void loggerTest() {
-		
+	public void actualTest() {
+		System.out.println("2. I am into Test annotated method");
 	}
 
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("3. I am into AfterMethod annotated method");
+	}
+	
 }
